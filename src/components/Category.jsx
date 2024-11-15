a296dbefc02fdd8c05d0d080692e45945db896a7
Shubham-Patel07 <shubham2107patel@gmail.com>
2024-11-16 00:49:14 +0530

    MealDB API called and data fetched sucessfully

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 //import icons
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
 
 export default function Category() {
-   
+    const [category,setCategory] = useState([]);
+
+    const fetchCategory = async () => {
+        const response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+        const data = await response.json();
+        setCategory(data.categories);
+    }
+
+    useEffect(
+        () => {
+            fetchCategory();
+        },[]
+    ) 
 
   return (
     <>
@@ -16,7 +28,19 @@ export default function Category() {
                 <div className='cursor-pointer flex items-center justify-center w-[30px] h-[30px] bg-[#02060c26] rounded-full mx-2'><FaArrowRight /></div>
             </div>
         </div>
-        
+        <div className='flex border border-green-500 overflow-hidden'>
+            {
+                category.map(
+                    (cat,index) => {
+                        return (
+                            <div key={index} className='w-[200px] shrink-0'>
+                                <img src={cat.strCategoryThumb} alt="" />
+                            </div>
+                        )
+                    }
+                )
+            }
+        </div>
     </div>
     </>
   )
