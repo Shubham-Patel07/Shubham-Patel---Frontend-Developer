2165aa6649c5ef68226fe7a3a0b5d0c0199447a4
Shubham-Patel07 <shubham2107patel@gmail.com>
2024-11-15 09:56:31 +0530

    Sidebar Added and list rendered for different links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-//import caret icon
+//import icons
 import { RxCaretDown } from "react-icons/rx";
+import { IoIosSearch } from "react-icons/io";
+import { CiDiscount1 } from "react-icons/ci";
+import { IoHelpBuoyOutline } from "react-icons/io5";
+import { IoPersonOutline } from "react-icons/io5";
+import { BsCart } from "react-icons/bs";
 
 //import useState
 import { useState } from 'react';
@@ -15,21 +20,67 @@ export default function Header() {
     setToggle(true);
   }
 
+  const hideSideMenu = () => {
+    setToggle(false);
+  }
+
+  const links =[
+    {
+      icon: <IoIosSearch />,
+      name: "Search"
+    },
+    {
+      icon: <CiDiscount1 />,
+      name: "Offers"
+    },
+    {
+      icon: <IoHelpBuoyOutline />,
+      name: "Help"
+    },
+    {
+      icon: <IoPersonOutline />,
+      name: "Sign In"
+    },
+    {
+      icon: <BsCart />,
+      name: "Cart"
+    },
+  ]
+
   return (
     <>
-      <div className='black-overlay w-full h-full fixed duration-500' style={{
+      <div className='black-overlay w-full h-full fixed duration-500' onClick={hideSideMenu} style={{
         opacity: toggle ? 1 : 0,
         visibility: toggle ? "visible" : "hidden"
-      }}></div>
+      }}>
+        <div onClick={(e) => {
+          e.stopPropagation();
+        }} className='w-[500px] bg-white h-full absolute duration-[400ms]' 
+        style={{
+          left: toggle ? '0%' : '-100%'
+        }}></div>
+      </div>
       <header className='p-[15px] shadow-xl'>
-        <div className='max-w-[1200px] mx-auto border border-red-500 flex items-center'>
-          <div className='w-[100px] border border-blue-500'>
+        <div className='max-w-[1200px] mx-auto flex items-center'>
+          <div className='w-[100px]'>
             <img src="images/logo.png" className='w-full' alt="" />
           </div>
           <div>
             <span className='font-bold border-b-[3px] border-[black]'> Baner </span> Pune, Maharashtra, India
             <RxCaretDown onClick={showSideMenu} fontSize={25} className='inline text-[#ff5200]' />
           </div>
+          <nav className='flex list-none gap-10 ml-auto text-[18px] font-semibold'>
+            {
+              links.map(
+                (link, index) => {
+                  return<li key={index} className='cursor-pointer flex hover:text-[#ff5200] items-center gap-2'>
+                    {link.icon}
+                    {link.name}
+                  </li>
+                }
+              )
+            }
+          </nav>
         </div>
       </header>
     </>
